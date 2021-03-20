3a689b35696670485b8174e9559a4444977a19cc
Iury Teixeira <franciury@gmail.com>
2021-03-20 15:12:39 -0300

    link to ddoc

--- src/main/java/com/bossabox/vuttr/api/controller/IndexController.java
+++ src/main/java/com/bossabox/vuttr/api/controller/IndexController.java
@@ -8,7 +8,8 @@ public class IndexController {
 
 	@GetMapping({ "", "/" })
 	public String index() {
-		return "redirect:/docs.html";
+		return "API OK. Para acessar a documentação <a href=\"/docs.html\">clique aqui</a>"
+				+ "";
 	}
 
 }
